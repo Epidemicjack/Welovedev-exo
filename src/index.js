0447b9d68acd526ac053825dd4c3a1c334906df5
Valentin Nortier <52537670+Epidemicjack@users.noreply.github.com>
2019-07-19 17:12:27 +0200

    ajout de la const "liste"

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ ReactDOM.render(
     test,
     document.getElementById('root')
 );
+
+const liste =
+<h1>WeLoveDev :</h1>
+<ul>
+  <li>Damien Cavaillès - Co-founder</li>
+  <li>Vincent Cotro - Co-founder</li>
+  <li>Thomas Grivet - React developer</li>
+  <li>Martin Lutton - Recruiter</li>
+  <li>Alexis Camus - Customer success</li>
+  <li>Nicolas Detrez - Content Specialist</li>
+  <li>Clement Devos - JS Developer</li>
+  <li>Quentin Tournier - React Developer</li>
+  <li>Pierre Willame - Customer success</li>
+  <li>Alexandre Brisbout - Customer success</li>
+  <li>Valentin Nortier - le mec qui essaye</li>
+
+</ul>
+;
